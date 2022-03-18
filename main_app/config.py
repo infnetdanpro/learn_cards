4531d28610dbc5748b2b898fd2a6d9b5e8e630f8
infnetdanpro <33713163+infnetdanpro@users.noreply.github.com>
2022-03-18 23:03:42 +0300

    Update config.py

--- main_app/config.py
+++ main_app/config.py
@@ -1,5 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://zlqhjvjyjmzugm:a190bc5c76686ae604ed86029f52bc37bdf5a26d44ba25679d93e17cd1ce0de4@ec2-99-80-73-211.eu-west-1.compute.amazonaws.com:5432/d2v9e9ddvak4ee
-"
+SQLALCHEMY_DATABASE_URI = "postgresql://zlqhjvjyjmzugm:a190bc5c76686ae604ed86029f52bc37bdf5a26d44ba25679d93e17cd1ce0de4@ec2-99-80-73-211.eu-west-1.compute.amazonaws.com:5432/d2v9e9ddvak4ee"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_SECRET_KEY = "asdasdas12312313-3-3--33_#_#_!@#(!@^$%()/=1"
 
